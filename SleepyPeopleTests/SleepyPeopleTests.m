48a2b7ee9e8c8dd6870cfc6db8729a0661c3e5a3
David Groulx <david@sandbendersoftware.com>
2014-06-03 10:03:48 -0400

    Initial Commit

---
+++ SleepyPeopleTests/SleepyPeopleTests.m
@@ -0,0 +1,34 @@
+//
+//  SleepyPeopleTests.m
+//  SleepyPeopleTests
+//
+//  Created by David Groulx on 6/3/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SleepyPeopleTests : XCTestCase
+
+@end
+
+@implementation SleepyPeopleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
